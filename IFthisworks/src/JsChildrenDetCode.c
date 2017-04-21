be87a12200c6ec77f280fac0a2544b97a4f70eec
Jordyn Hartzell <hartzellj@student.swosu.edu>
2017-04-21 13:07:03 -0500

    cleaning code

--- IFthisworks/src/JsChildrenDetCode.c
+++ IFthisworks/src/JsChildrenDetCode.c
@@ -4,7 +4,8 @@
 #include <omp.h>
 int nthreads = 4;
 
-char inputDataFileName[50]; // declares the array to hold our binary input file name.
+// declaration to hold binary input file
+char inputDataFileName[50];
 
 
 #define ARRAYSIZE 5000
@@ -25,8 +26,6 @@ double addAsWeGoLogAbsDet = 0;
 void readDataFromDotBinIntoMatrix ();
 void makeUpperTriangularFunction ();
 void multiplyDiagonalAndReturnAnswer ();
-//void printMatrix();
-
 
 int main(void) {
 
@@ -47,16 +46,6 @@ int main(void) {
 // function to create upper triangle of 0's
 void makeUpperTriangularFunction ()
 {
-
-	/*
-	 * Our pattern for using the pointer is to start out with the
-	 * arrayPointer address.
-	 * Then we add to that the number of rows times the array size.
-	 * This gives us the offset from the beginning of the vector to grab
-	 * our current row.
-	 * Next we add our column address as an additional offset.
-	 * This should park us right at the number we are looking for.
-	 */
 	
 	// initialize variable for row reduction
 	double rowReductionConstant = 0;
@@ -68,28 +57,10 @@ void makeUpperTriangularFunction ()
 		// work through each row
 		for(int rowToWorkOn = (columnToGetZeros+1); rowToWorkOn < ARRAYSIZE; rowToWorkOn++)
 		{
-			//printf("\tWe will work on row %d\n.", rowToWorkOn);
-			//printf("\t\tWe will subtract from %d a constant times row %d\n", rowToWorkOn, columnToGetZeros);
-			//printf("\t\t\tThat constant is [row, column] [%d, %d] divided by [row, column] [%d, %d]\n",
-			//rowToWorkOn, columnToGetZeros, columnToGetZeros, columnToGetZeros);
-			
-			//*********************************************************************************************************
 			rowReductionConstant =
 					*(arrayPointer + rowToWorkOn * ARRAYSIZE + columnToGetZeros )
 					/ *(arrayPointer + columnToGetZeros * ARRAYSIZE + columnToGetZeros );
-			//printf("\tWe will work on row %d\n.", rowToWorkOn);
-			//printf("\t\tWe will subtract from %d a constant, %f, times row %d\n", rowToWorkOn, rowReductionConstant, columnToGetZeros);
-			//printf("\t\t\tThat constant is [%d, %d]/[%d, %d], or %f.\n",
-			//	rowToWorkOn, columnToGetZeros, columnToGetZeros, columnToGetZeros, rowReductionConstant);
-
-			/* now we apply the multiplication.
-			 * We will work across the entire row to work on.
-			 * Our new row will be our old row minus the row
-			 * number which happens to be the same number as the
-			 * column we are trying to turn into zeros.
-			 * We will use a for loop for now for this process.
-			 * Later, we can use a openMP parallel for loop.
-			 */
+			
 // use a pragma to initiate openMP			
 #pragma omp parallel for
 
@@ -121,12 +92,7 @@ void readDataFromDotBinIntoMatrix ()
 
 	// use malloc to assign array
 	arrayPointer = (double *)malloc(ARRAYSIZE * ARRAYSIZE * sizeof(double));
-	// here, our array pointer is called arrayPointer.
-	// our type is double.
-	// our size is number of rows times the number of columns times the size of doubles.
-	// we have ARRAYSIZE because we will always use a square matrix.
-
-
+	
 	// set the array to zeros
 	for(int rowCounter = 0; rowCounter <ARRAYSIZE; rowCounter++)
 	{
@@ -141,75 +107,42 @@ void readDataFromDotBinIntoMatrix ()
 		}
 	}
 
-	// read in array to pointer memory allocated space full of zeros.
-
-	//char inputDataFileName[50];
-
-	// open file
-	//sprintf(inputDataFileName,"m0016x0016.bin");
-	
-	//sprintf(inputDataFileName,"m0496x0496.bin");
 	sprintf(inputDataFileName,FileNameString);
 	
 	printf("Reading array file %s of size %dx%d\n",inputDataFileName,ARRAYSIZE,ARRAYSIZE);
-	//Open file
+	// open file
 	FILE *inputDataFile=fopen(inputDataFileName,"rb");
-	//Read elelements
 
+	// read data from file
 	for(int rowCounter = 0; rowCounter <ARRAYSIZE; rowCounter++)
 	{
 		for(int columnCounter = 0; columnCounter < ARRAYSIZE; columnCounter++)
 		{
-			/* Where the array starts
-			 * plus our row counter times the number of elements in our array
-			 * plus our column counter
-			 * is set to zero.
-			 */
-			//*( arrayPointer + rowCounter * ARRAYSIZE + columnCounter ) = 0;
 			fread((arrayPointer + rowCounter * ARRAYSIZE + columnCounter),sizeof(double),1,inputDataFile);
 		}
 	}
 
-	// print the matrix to a csv file.
-	//printMatrix();
-
 	printf("Finished reading array file %s of size %dx%d\n",inputDataFileName,ARRAYSIZE,ARRAYSIZE);
 
-} /* readDataFromDotBinIntoMatrix */
+} // end readData
 
 
 void multiplyDiagonalAndReturnAnswer ()
 {
-	printf("Hello from multiply diagonal and return answer function.\n");
-
-	/*
-	 * For this exercise, we will multiply across the diagonal.
-	 * We will start at [0,0] and multiply it by [1,1].
-	 * We could use an openMP reduction for this later on.
-	 * For now, we will just do it with a for loop.
-	 *
-	 * To access this number, we begin with the address of the arrayPointer.
-	 * We add to that the row we want times the array size.
-	 * This is the offset to get us onto the row we want to work on.
-	 * Then we add to that the number of columns we want to go over.
-	 * That parks us at the number we want to grab.
-	 */
-
+	
+	// iterate through diagonals of array
 	for(int diagonalAddress = 0; diagonalAddress < ARRAYSIZE; diagonalAddress++)
 	{
+		// mutiply diagonal nums
 		diagonalMultiplierVariableLongDouble = diagonalMultiplierVariableLongDouble *
 				(long double)*(arrayPointer + diagonalAddress * ARRAYSIZE + diagonalAddress );
-
-		/* Now we try to add as we go.
-		 *
-		 */
-
+		
+		// take log of diagonal num and add to total
 		addAsWeGoLogAbsDet = addAsWeGoLogAbsDet +
 				log10(fabs(*(arrayPointer + diagonalAddress * ARRAYSIZE + diagonalAddress )));
 	}
 
-	//printf("\nThis provides a log of the absolute value of the determinant of: %e.\n", logOfAbsOfDet);
-	
+	// output results	
 	printf("\nThe diagonal multiplied all together is: %Le.\n",diagonalMultiplierVariableLongDouble);
 	printf("Rounded to a double, this is: %e.\n",(double)diagonalMultiplierVariableLongDouble);
 	printf("Add as we go of the log of the abs of the det is %e.\n", addAsWeGoLogAbsDet);
@@ -219,5 +152,5 @@ void multiplyDiagonalAndReturnAnswer ()
 	
 
 	return;
-} /* multiplyDiagonalAndReturnAnswer */
+} // end diagonalAnswer
 
